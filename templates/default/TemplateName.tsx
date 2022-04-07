8f811e90f63a40f17351e77e3d168d7a241838ef
Andrey Nekrasov <bonerdelli@gmail.com>
2022-04-07 17:05:41 +0300

    Add code generation

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd'
+
+import './TemplateName.style.less'
+
+const { Paragraph } = Typography
+
+const TemplateName = () => (
+  <div className="TemplateName" data-testid="TemplateName">
+    <Paragraph>TemplateName component</Paragraph>
+  </div>
+)
+
+export default TemplateName
