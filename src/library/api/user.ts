4dbad49d9018ccddb241664e93d10915deee597b
Andrey Nekrasov <bonerdelli@gmail.com>
2022-04-08 19:28:37 +0300

    Add API helpers and some mocks

---
+++ src/library/api/user.ts
@@ -0,0 +1,11 @@
+import { ApiErrorResponse /* , get */ } from 'library/helpers/api'
+import { User } from 'library/models'
+
+import mockData from 'library/mock/user'
+
+export async function getCurrentUser(/* id: string */): Promise<
+  User | ApiErrorResponse
+> {
+  // const result = await get<User | ApiErrorResponse>('user/current') // NOTE: sample API call
+  return mockData
+}
