8543482cd5f5f32c64e52724f58ccb19b8c37718
Andrey Nekrasov <bonerdelli@gmail.com>
2022-04-19 15:55:12 +0500

    Debug embedding of SVG image

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { Layout } from 'antd'
 
 import './AppHeader.style.less'
+import Logo from 'orient-ui-library/assets/orient-logo.svg'
 import config from 'config/portal.yaml'
 
 const { Header } = Layout
@@ -9,9 +10,12 @@ const { Header } = Layout
 const AppHeader = () => {
   const { sections } = config
   const { t } = useTranslation()
+  console.log('Init sections', sections, t('shortTitle'))
   return (
     <Header className="AppHeader">
-      <div className="logo" />
+      <div className="AppHeader__logo">
+        <Logo width={100} height={100} />
+      </div>
     </Header>
   )
 }
