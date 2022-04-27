139d9ff4fbf5eacebe3fd8f1731c38a9200db5e3
Andrey Nekrasov <bonerdelli@gmail.com>
2022-04-27 11:54:55 +0300

    Add common error components

---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Result, Button } from 'antd'
+
+type PageNotFoundProps = {
+  title?: string
+  message?: string
+  navigateBackLabel?: string
+  navigateBack?: () => void
+}
+
+export const PageNotFound: React.FC<PageNotFoundProps> = ({
+  title = '404',
+  message = 'Запрашиваемая страница не найдена',
+  navigateBackLabel = 'На главную',
+  navigateBack,
+}) => (
+  <Result
+    status="404"
+    title={title}
+    subTitle={message}
+    extra={navigateBack && [
+      <Button key="reload" onClick={() => navigateBack()}>
+        {navigateBackLabel}
+      </Button>,
+    ]}
+  />
+)
