4c008bea228133cb5a6c14e7549dd337af89922b
Andrey Nekrasov <bonerdelli@gmail.com>
2022-04-29 15:30:42 +0300

    ORIENT-13 Add form generation

--- src/components/CompanyForm/CompanyForm.tsx
+++ src/components/CompanyForm/CompanyForm.tsx
@@ -7,39 +7,71 @@ import './CompanyForm.style.less'
 
 // const { Paragraph } = Typography
 
+type FormInputConfig = [
+  string, // Model
+  string, // Name
+  boolean, // Is Required
+  boolean? // Is Editable
+]
+
 const CompanyForm = () => {
   const { t } = useTranslation()
   const requiredRule = {
     required: true,
   }
-  const renderTextInput = (name: string, isRequired: boolean) => (
+  const renderTextInput = (model: string, field: string, isRequired: boolean, isEditable = false) => (
     <FormItem
-      key={name}
-      name={name}
-      label={t(`models.company.fields.${name}.title`)}
+      key={`${model}.${field}`}
+      name={`${model}.${field}`}
+      label={t(`models.${model}.fields.${field}.title`)}
       rules={isRequired ? [requiredRule] : []}
     >
-      <Input />
+      <Input disabled={!isEditable} />
     </FormItem>
   )
+  const renderTextInputs = (inputConfig: FormInputConfig[]) => inputConfig.map(item => renderTextInput(...item))
   const renderMainSection = () => (
     <Card title={t('сompanyPage.formSections.main.title')}>
-      {renderTextInput('fullName', true)}
+      {renderTextInputs([
+        [ 'company', 'fullName', true ],
+        [ 'company', 'shortName', false, true ],
+        [ 'company', 'inn', true ],
+        [ 'company', 'opf', true ],
+        // TODO: make a two-cols layout
+        [ 'company', 'isMsp', true ],
+        [ 'company', 'capital', true ],
+        [ 'company', 'currency', true ],
+        [ 'company', 'oked', true ],
+        [ 'company', 'soogu', true ],
+        [ 'company', 'state', true ],
+      ])}
     </Card>
   )
   const renderContacts = () => (
     <Card title={t('сompanyPage.formSections.contacts.title')}>
-
+      {renderTextInputs([
+        [ 'companyContact', 'primaryEmail',    false ],
+        [ 'companyContact', 'additionalEmail', false ],
+        [ 'companyContact', 'primaryPhone',    true ],
+        [ 'companyContact', 'additionalPhone', true ],
+      ])}
     </Card>
   )
   const renderRegAuthority = () => (
     <Card title={t('сompanyPage.formSections.regAuthority.title')}>
-
+      {renderTextInputs([
+        [ 'company', 'regAuthority', true ],
+        [ 'company', 'regDate', true ],
+        [ 'company', 'regNumber', true ],
+      ])}
     </Card>
   )
   const renderFounder = () => (
     <Card title={t('сompanyPage.formSections.founder.title')}>
-
+      {renderTextInputs([
+        [ 'companyFounder', 'lastName', true ], // TODO: ask for compound field
+        [ 'companyFounder', 'inn', false ],
+      ])}
     </Card>
   )
 
