69e7f30f69ea6113eb578bae24a42e7b92f962d6
Andrey Nekrasov <bonerdelli@gmail.com>
2022-05-11 21:32:11 +0300

    ORIENT-14 Update layout and styles

--- src/components/CompanyHeadsList/CompanyHeadsList.tsx
+++ src/components/CompanyHeadsList/CompanyHeadsList.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Typography } from 'antd'
+import { Table, Tag, Space } from 'antd'
+import type { ColumnsType } from 'antd/lib/table'
 
 import './CompanyHeadsList.style.less'
 
-const { Paragraph } = Typography
+interface CompanyHead {
+  fullName: string
+  isExecutive: boolean
+  isAttorney: boolean
+  ownership: boolean
+}
 
 export interface CompanyHeadsListProps {
 
@@ -11,9 +18,34 @@ export interface CompanyHeadsListProps {
 
 const CompanyHeadsList: React.FC<CompanyHeadsListProps> = ({}) => {
   const { t } = useTranslation()
+  const [ data, setData ] = useState<CompanyHead[]>()
+
+  const columns: ColumnsType<CompanyHead> = [
+    {
+      title: t('headsPage.tableColumns.fullName'),
+      dataIndex: 'fullName',
+      key: 'fullName',
+    },
+    {
+      title: t('headsPage.tableColumns.isExecutive'),
+      dataIndex: 'isExecutive',
+      key: 'isExecutive',
+    },
+    {
+      title: t('headsPage.tableColumns.isAttorney'),
+      dataIndex: 'isAttorney',
+      key: 'isAttorney',
+    },
+    {
+      title: t('headsPage.tableColumns.ownership'),
+      dataIndex: 'ownership',
+      key: 'ownership',
+    },
+  ]
+
   return (
     <div className="CompanyHeadsList" data-testid="CompanyHeadsList">
-      <Paragraph>{t('CompanyHeadsList.component')}</Paragraph>
+      <Table columns={columns} dataSource={data} />
     </div>
   )
 }
--- src/pages/CompanyHeadsPage/CompanyHeadsPage.tsx
+++ src/pages/CompanyHeadsPage/CompanyHeadsPage.tsx
@@ -1,15 +1,18 @@
 import { useTranslation } from 'react-i18next'
 import { Layout, Typography } from 'antd'
 
+import CompanyHeadsList from 'components/CompanyHeadsList'
+
 import './CompanyHeadsPage.style.less'
 
-const { Paragraph } = Typography
+const { Title } = Typography
 
 const CompanyHeadsPage = () => {
   const { t } = useTranslation()
   return (
     <Layout className="CompanyHeadsPage" data-testid="CompanyHeadsPage">
-      <Paragraph>{t('CompanyHeadsPage.component')}</Paragraph>
+      <Title level={3}>{t('sections.heads.title')}</Title>
+      <CompanyHeadsList />
     </Layout>
   )
 }
