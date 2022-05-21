03fd79a4794dfd94cafa61b7737f0d3dccb1f5bf
Andrey Nekrasov <bonerdelli@gmail.com>
2022-05-21 22:13:47 +0300

    Debug uding of common components

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { Layout, Spin } from 'antd'
+import { Spin } from 'antd'
 
 import {
   User,
@@ -9,7 +9,7 @@ import {
   healthCheck,
 } from 'orient-ui-library/library'
 
-import { ErrorResultView } from 'orient-ui-library/components'
+import { Div, ErrorResultView } from 'orient-ui-library/components'
 
 import { useStoreActions, useStoreState } from 'library/store'
 
@@ -19,7 +19,7 @@ import AppLayoutProtected from './AppLayoutProtected'
 import './AppLayout.style.less'
 
 const AppLayout = () => {
-
+//
   const user = useStoreState(state => state.user.currentUser)
   const { setCurrentUser } = useStoreActions(actions => actions.user)
   // TODO: loading state for user
@@ -39,7 +39,7 @@ const AppLayout = () => {
   const loadHealthStatus = async () => {
     const healthStatus = await healthCheck()
     if (!healthStatus) {
-      setApiError('common.errors.apiUnavailable')
+      setApiError('common.errors.apiUnavailable') // FIXME: error component not loading
     }
     return healthStatus
   }
@@ -55,21 +55,23 @@ const AppLayout = () => {
     initialize()
   }, [])
 
-  if (apiError) {
-    <Layout className="AppLayout__globalError">
-      <ErrorResultView
-        title="common.errors.apiUnavailable.title"
-        message={`${apiError}.desc`}
-        status="error"
-      />
-    </Layout>
-  }
+  // if (apiError) {
+  //   return (
+  //     <Div className="AppLayout__globalError">
+  //       <ErrorResultView
+  //         title="common.errors.apiUnavailable.title"
+  //         message={`${apiError}.desc`}
+  //         status="error"
+  //       />
+  //     </Div>
+  //   )
+  // }
 
   if (loading) {
     return (
-      <Layout className="AppLayout__loaderWrap">
+      <Div className="AppLayout__loaderWrap">
         <Spin size="large" />
-      </Layout>
+      </Div>
     )
   }
 
