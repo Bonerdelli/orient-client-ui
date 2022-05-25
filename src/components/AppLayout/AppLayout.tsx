ce6704f7a9a1e8faf47ba647af67dc72088d0679
Andrey Nekrasov <bonerdelli@gmail.com>
2022-05-25 10:22:01 +0300

    Resolve importing issue with local file link

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Spin } from 'antd'
 
 import { healthCheck } from 'orient-ui-library/library'
-import ErrorResultView from 'ui-components/ErrorResultView'
-import Div from 'ui-components/Div'
+import ErrorResultView from 'orient-ui-library/components/ErrorResultView'
+import Div from 'orient-ui-library/components/Div'
 
 import { useStoreActions, useStoreState } from 'library/store'
 
