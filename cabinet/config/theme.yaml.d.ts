62eb50fea4150bb17338bf719575b1fdae62e85a
Andrey Nekrasov <bonerdelli@gmail.com>
2022-06-29 23:51:32 +0300

    Add missed typings

---
+++ cabinet/config/theme.yaml.d.ts
@@ -0,0 +1,12 @@
+declare const themeConfig: {
+  'layout-header-color': string
+  'layout-header-background': string
+  'layout-header-height': string
+  'typography-title-font-weight': number
+  'typography-title-margin-top': string
+  'primary-color': string
+  'side-navigation-width': number
+  'side-navigation-collapsed-width': number
+}
+
+export default themeConfig
