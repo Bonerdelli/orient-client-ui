d8e543a56566ee1031a792819794391c06a99e0c
Maxim Tselikin <phanock@gmail.com>
2022-06-29 23:25:31 +0300

    ORIENT-176. Add company founder modal

--- operator/src/library/models/companyHead.ts
+++
@@ -1,5 +0,0 @@
-import { CompanyFounderDto as CompanyHeadApiModel } from 'orient-ui-library/library/models/proxy'
-
-export interface CompanyHead extends CompanyHeadApiModel {
-  fullName: string
-}
