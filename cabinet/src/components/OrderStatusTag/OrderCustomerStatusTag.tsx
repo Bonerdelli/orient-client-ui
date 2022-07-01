6eb45e741e943095a5ddc10290287b471a6b4461
Andrey Nekrasov <bonerdelli@gmail.com>
2022-07-01 20:43:38 +0300

    Separate statuses for Customer and Client

---
+++ cabinet/src/components/OrderStatusTag/OrderCustomerStatusTag.tsx
@@ -0,0 +1,40 @@
+import { useTranslation } from 'react-i18next'
+import { Tag, Button } from 'antd'
+import { ReloadOutlined } from '@ant-design/icons'
+
+import { Order, OrderStatus } from 'orient-ui-library/library/models/order'
+
+import './OrderStatusTag.style.less'
+
+export interface OrderStatusTagProps {
+  statusCode?: OrderStatus,
+  refreshAction?: () => void
+  item?: Order,
+}
+
+const OrderStatusTag: React.FC<OrderStatusTagProps> = ({ statusCode, refreshAction }) => {
+  const { t } = useTranslation()
+  let refreshButton: JSX.Element | null = null
+  if (refreshAction) {
+    refreshButton = (
+      <Button
+        className="OrderStatusTag__refreshButton"
+        icon={<ReloadOutlined />}
+        onClick={refreshAction}
+        type="link"
+        size="small"
+      />
+    )
+  }
+  switch (statusCode) {
+    case OrderStatus.FRAME_CUSTOMER_SIGN:
+      return <Tag color="green">{t('orderStatusCustomerTitles.customerSign')}{refreshButton}</Tag>
+    case OrderStatus.FRAME_COMPLETED:
+      return <Tag color="blue">{t('orderStatusCustomerTitles.completed')}{refreshButton}</Tag>
+    default:
+      // NOTE: unknown statutes shouldn't be displayed
+      return <></> // <Tag>{item.statusName}{refreshButton}</Tag>
+  }
+}
+
+export default OrderStatusTag
