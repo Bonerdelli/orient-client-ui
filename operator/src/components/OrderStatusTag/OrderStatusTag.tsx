f61a0d2daa3acbea30b385daff0f89a8f85d8738
Andrey Nekrasov <bonerdelli@gmail.com>
2022-07-05 12:23:55 +0300

    Update factor status rendering

--- operator/src/components/OrderStatusTag/OrderStatusTag.tsx
+++ operator/src/components/OrderStatusTag/OrderStatusTag.tsx
@@ -2,12 +2,16 @@ import { useTranslation } from 'react-i18next'
 import { Tag, Button } from 'antd'
 import { ReloadOutlined } from '@ant-design/icons'
 
-import { Order, OrderStatus } from 'orient-ui-library/library/models/order'
+import {
+  Order,
+  OrderStatus,
+  FactoringStatus,
+} from 'orient-ui-library/library/models/order'
 
 import './OrderStatusTag.style.less'
 
 export interface OrderStatusTagProps {
-  statusCode?: OrderStatus,
+  statusCode?: OrderStatus | FactoringStatus,
   refreshAction?: () => void
   item?: Order,
 }
@@ -31,10 +35,13 @@ const OrderStatusTag: React.FC<OrderStatusTagProps> = ({
 
   switch (statusCode) {
     case OrderStatus.FRAME_OPERATOR_WAIT_FOR_VERIFY:
+    case FactoringStatus.FACTOR_OPERATOR_WAIT_FOR_VERIFY:
       return <Tag color="green">{t('orderStatusTitles.waitForVerify')}{refreshButton}</Tag>
     case OrderStatus.FRAME_OPERATOR_VERIFYING:
+    case FactoringStatus.FACTOR_OPERATOR_VERIFY:
       return <Tag color="green">{t('orderStatusTitles.verifying')}{refreshButton}</Tag>
     case OrderStatus.FRAME_CLIENT_SIGN:
+    case FactoringStatus.FACTOR_CLIENT_SIGN:
       return <Tag color="blue">{t('orderStatusTitles.clientSign')}{refreshButton}</Tag>
     case OrderStatus.FRAME_CLIENT_REWORK:
       return <Tag color="blue">{t('orderStatusTitles.needsForRework')}{refreshButton}</Tag>
@@ -43,12 +50,17 @@ const OrderStatusTag: React.FC<OrderStatusTagProps> = ({
     case OrderStatus.FRAME_HAS_OFFER:
       return <Tag color="blue">{t('orderStatusTitles.hasOffer')}{refreshButton}</Tag>
     case OrderStatus.FRAME_CUSTOMER_SIGN:
+    case FactoringStatus.FACTOR_CUSTOMER_SIGN:
       return <Tag color="blue">{t('orderStatusTitles.customerSign')}{refreshButton}</Tag>
+    case OrderStatus.FRAME_COMPLETED:
+    case FactoringStatus.FACTOR_COMPLETED:
+      return <Tag color="blue">{t('orderStatusTitles.completed')}</Tag>
     case OrderStatus.FRAME_CANCEL:
+    case FactoringStatus.FACTOR_CANCEL:
       return <Tag>{t('orderStatusTitles.cancel')}</Tag>
     case OrderStatus.FRAME_OPERATOR_REJECT:
+    case FactoringStatus.FACTOR_OPERATOR_REJECT:
       return <Tag color="red">{t('orderStatusTitles.operatorReject')}</Tag>
-    case OrderStatus.FRAME_COMPLETED:
     default:
       return <Tag>{item?.statusName}</Tag>
   }
