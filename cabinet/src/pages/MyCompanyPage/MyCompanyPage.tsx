9e6a346e98f60630b3fc97ca71c7ed6a46460361
Maxim Tselikin <phanock@gmail.com>
2022-07-07 17:00:09 +0300

    ORIENT-181. Добавил подтягивание фаундеров и поиск среди них ИО

--- cabinet/src/pages/MyCompanyPage/MyCompanyPage.tsx
+++ cabinet/src/pages/MyCompanyPage/MyCompanyPage.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next'
-import { Layout, Tabs, Spin } from 'antd'
+import { Layout, message, Spin, Tabs } from 'antd'
 
 import { useStoreState } from 'library/store'
 
@@ -9,6 +9,9 @@ import CompanyForm from 'components/CompanyForm'
 import CompanyContactsForm from 'components/CompanyContactsForm'
 
 import './MyCompanyPage.style.less'
+import { useEffect, useState } from 'react'
+import { getCompanyHeads } from 'library/api'
+import { CompanyFounderDto } from 'orient-ui-library/library/models/proxy'
 
 const { TabPane } = Tabs
 
@@ -16,10 +19,33 @@ const MyCompanyPage = () => {
   const { t } = useTranslation()
   const company = useStoreState(state => state.company.current)
 
-  if (!company) {
+  const [ companyHeadName, setCompanyHeadName ] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (company) {
+      loadCompanyHeadData()
+    }
+  }, [ company ])
+
+  const loadCompanyHeadData = async () => {
+    const result = await getCompanyHeads({ companyId: company!.id! })
+    if (result.success) {
+      const io = (result.data as CompanyFounderDto[]).find(({ isIo }) => isIo)
+      if (!io) {
+        message.error('У компании отсутсвует учредитель')
+      } else {
+        const { firstName, lastName, secondName } = io
+        setCompanyHeadName(`${firstName} ${lastName} ${secondName}`)
+      }
+    } else {
+      message.error('Ошибка получения информации об учредителе компании')
+    }
+  }
+
+  if (!company || !companyHeadName) {
     return (
       <Div className="AppLayout__loaderWrap">
-        <Spin size="large" />
+        <Spin size="large"/>
       </Div>
     )
   }
@@ -28,10 +54,10 @@ const MyCompanyPage = () => {
     <Layout className="MyCompanyPage" data-testid="MyCompanyPage">
       <Tabs className="MyCompanyPage__tabNavigation">
         <TabPane tab={t('companyPage.tabs.mainInfo.title')} key="main-info">
-          <CompanyForm company={company} />
+          <CompanyForm company={company} companyFounderFullName={companyHeadName}/>
         </TabPane>
         <TabPane tab={t('companyPage.tabs.contacts.title')} key="contacts">
-          <CompanyContactsForm companyId={company.id as number} />
+          <CompanyContactsForm companyId={company.id as number}/>
         </TabPane>
       </Tabs>
     </Layout>
