7daa1198b829150fcc858951c6e0865c724b2073
Andrey Nekrasov <bonerdelli@gmail.com>
2022-08-02 22:32:44 +0300

    ORIENT-234 Update reject modal behaviour

--- operator/src/components/RejectOrderModal/RejectOrderModal.tsx
+++ operator/src/components/RejectOrderModal/RejectOrderModal.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Modal, Form, Select, Input } from 'antd'
+import { Modal, Form, Select, Input, message } from 'antd'
+import type { BaseOptionType } from 'antd/es/select'
 
 import { useStoreState } from 'library/store'
 import './RejectOrderModal.style.less'
 
-const { useForm, Item: FormItem } = Form
-const { Option } = Select
+const { Item: FormItem } = Form
 const { TextArea } = Input
 
 const REASONS_DICTS_NAME = 'taxationSystem'
@@ -14,7 +14,7 @@ const REASONS_DICTS_NAME = 'taxationSystem'
 export interface RejectOrderModalProps {
   opened: boolean
   setOpened: (opened: boolean) => void
-  rejectHandler: (code: number, reason: string) => Promise<boolean>
+  rejectHandler: (reasonCode: number, comment?: string) => Promise<boolean>
 }
 
 const RejectOrderModal: React.FC<RejectOrderModalProps> = ({
@@ -25,48 +25,63 @@ const RejectOrderModal: React.FC<RejectOrderModalProps> = ({
   const { t } = useTranslation()
 
   const dictionaries = useStoreState(state => state.dictionary.list)
+
+  const [ reasons, setReasons ] = useState<BaseOptionType[]>([])
   const [ reasonCode, setReasonCode ] = useState<number>()
   const [ comment, setComment ] = useState<string>()
 
-  const handleReject = () => {
-    rejectHandler(0, '')
+  useEffect(() => {
+    if (dictionaries?.[REASONS_DICTS_NAME]) {
+      const options = dictionaries[REASONS_DICTS_NAME].map(datum => ({
+        label: datum.value,
+        value: datum.id,
+      }))
+      setReasons(options)
+    }
+  }, [dictionaries])
+
+  const handleReject = async () => {
+    if (reasonCode) {
+      const result = await rejectHandler(reasonCode, comment)
+      if (!result) {
+        message.error(t('common.errors.requestError.title'))
+      }
+      handleClose()
+    }
   }
 
-  console.log('reasonCode', reasonCode)
-  console.log('comment', comment)
+  const handleClose = () => {
+    setOpened(false)
+    setReasonCode(undefined)
+    setComment(undefined)
+  }
 
   const renderModalContent = () => (
     <Form layout="vertical">
       <FormItem name="reasonCode"
                 label={t('rejectOrderModal.fields.reason')}
-
                 rules={[{ required: true }]}>
         <Select loading={!dictionaries}
-                onSelect={(value: number) => setReasonCode(value)}>
-          {dictionaries?.[REASONS_DICTS_NAME]?.map(item => (
-            <Option key={item.id} value={item.id}>
-              {item.value}
-            </Option>
-          ))}
-        </Select>
+                options={reasons}
+                onSelect={(value: number) => setReasonCode(value)} />
       </FormItem>
       <FormItem name="comment"
-                label={t('rejectOrderModal.fields.comment')}
-                >
+                label={t('rejectOrderModal.fields.comment')}>
         <TextArea rows={6} onChange={e => setComment(e.target.value)} />
       </FormItem>
     </Form>
   )
+
   return (
     <Modal title={t('rejectOrderModal.title')}
            visible={opened}
            onOk={handleReject}
+           onCancel={handleClose}
            okText={t('common.actions.reject.title')}
            okButtonProps={{
              disabled: !reasonCode,
              danger: true,
-           }}
-           onCancel={() => setOpened(false)}>
+           }}>
       {renderModalContent()}
     </Modal>
   )
